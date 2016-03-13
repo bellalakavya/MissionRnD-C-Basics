5f721814fc3b42b99c4ea3c4334eaebf61b7e63b
bellalakavya <bellalakavya@missionrnd.com>
2016-03-13 14:33:23 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,27 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+	int i, f = 0;
+	if (num1 != 0)
+	{
+		if (num1 < num2)
+		{
+			if (num1 > 0 && num2 > 0)
+			{
+				for (i = num1; i <= num2; i++)
+				{
+					if (i%num1 == 0)
+					{
+						f++;
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		return -1;
+	}
+	return f;
+	
 }
